29c41a62a8dba0395a0ed05c6569206cddbdf4c7
thadtastic <leothaddeus92@gmail.com>
2025-09-21 09:00:01 -0500

    registered models and corrected error

--- maintenance_tracker/maintenance/models.py
+++ maintenance_tracker/maintenance/models.py
@@ -13,20 +13,20 @@ class Vehicle(models.Model):
     def __str__(self):
         return f"{self.year} {self.make} {self.model}"
     
-    class MaintenanceRecord(models.Model):
-        SERVICE_TYPES = [
-            ('oil_change', 'Oil change'),
-            ('tire_rotation', 'Tire Rotation'),
-            ('tire_pressure', 'Tire Pressure Check'),
-            ('fluid_check', 'Fluid Level Check'),
-            #('notes', 'Notes')
+class MaintenanceRecord(models.Model):
+    SERVICE_TYPES = [
+        ('oil_change', 'Oil change'),
+        ('tire_rotation', 'Tire Rotation'),
+        ('tire_pressure', 'Tire Pressure Check'),
+        ('fluid_check', 'Fluid Level Check'),
+        #('notes', 'Notes')
         ]
 
-        vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name="maintenance_records")
-        service_type = models.CharField(max_length=20,choices=SERVICE_TYPES)
-        date = models.DateField()
-        mileage = models.IntegerField()
-        notes = models.TextField(blank=True, null=True)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name="maintenance_records")
+    service_type = models.CharField(max_length=20,choices=SERVICE_TYPES)
+    date = models.DateField()
+    mileage = models.IntegerField()
+    notes = models.TextField(blank=True, null=True)
 
-        def __str__(self):
-            return f"{self.get_service_type_display()} - {self.date}"
+    def __str__(self):
+        return f"{self.vehicle} - {self.get_service_type_display()} ({self.date})"
